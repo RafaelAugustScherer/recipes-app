156c19769802d919d0f94c89f26a457bb3685bdf
Lygia Dias <lygiiadias@gmail.com>
2022-01-07 18:04:52 -0300

    Pagina explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function Explorar() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explorar" />
-      <div />
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Bebidas
+      </button>
       <MenuInferior />
     </>
   );
