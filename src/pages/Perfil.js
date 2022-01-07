bd7a578d1bc75daa2108a5952f344f095cedb863
Lygia Dias <lygiiadias@gmail.com>
2022-01-07 17:39:19 -0300

    Pagina de perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,14 +1,61 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function Perfil() {
+  const userEmail = JSON.parse(localStorage.getItem('user'));
+  const { email } = userEmail;
+  const history = useHistory();
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header title="Perfil" />
-      <div>
-        Profile
-      </div>
+
+      <p data-testid="profile-email">
+
+        Email:
+        {' '}
+        { email }
+      </p>
+
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/receitas-feitas') }
+
+      >
+        {' '}
+        Receitas Feitas
+        {' '}
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/receitas-favoritas') }
+      >
+        {' '}
+        Receitas Favoritas
+        {' '}
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleClick }
+
+      >
+        {' '}
+        Sair
+        {' '}
+
+      </button>
       <MenuInferior />
     </>
   );
