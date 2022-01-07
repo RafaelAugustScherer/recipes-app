6418a9e43b170345279024397de64afbae5d26ed
Lygia Dias <lygiiadias@gmail.com>
2022-01-07 18:48:21 -0300

    paginas de explorar

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function ExplorarBebidas() {
+  const history = useHistory();
+
   return (
     <>
       <Header title="Explorar Bebidas" />
-      <div>
-        ExplorarBebidas
-      </div>
+
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Me Surpreenda!
+      </button>
+
       <MenuInferior />
     </>
   );
--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function ExplorarComidas() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explorar Comidas" />
-      <div>
-        ExplorarComidas
-      </div>
+
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        // onClick={ () => history.push('/explorar/comidas') }
+      >
+        Me Surpreenda!
+      </button>
+
       <MenuInferior />
     </>
   );
