d004eda47ad340e1d97254b7efc8df39716baa18
Lygia Dias <lygiiadias@gmail.com>
2022-01-11 14:11:04 -0300

    correções

--- src/pages/Comes.js
+++ src/pages/Comes.js
@@ -6,40 +6,38 @@ import CategoryFilters from '../components/CategoryFilters';
 import MenuInferior from '../components/MenuInferior';
 
 function Comes() {
-  const { comidas, mealData } = useContext(RecipesContext);
+  const { comidas } = useContext(RecipesContext);
 
-  if (!mealData) {
-    return (
-      <>
-        <Header title="Comidas" comidasOuBebidas="comidas" />
-        <div>
-          <CategoryFilters comidasOuBebidas="comidas" />
-          {
-            comidas.map((comida, index) => {
-              const { id, name, image } = comida;
-              const MAX_LENGTH = 12;
-              if (index < MAX_LENGTH) {
-                return (
-                  <Card
-                    key={ name }
-                    thumb={ image }
-                    name={ name }
-                    index={ index }
-                    id={ id }
-                    url="comidas"
-                  />
-                );
-              }
-              return null;
-            })
-          }
-        </div>
+  return (
+    <>
+      <Header title="Comidas" comidasOuBebidas="comidas" />
+      <div>
+        <CategoryFilters comidasOuBebidas="comidas" />
+        {
+          comidas.map((comida, index) => {
+            const { id, name, image } = comida;
+            const MAX_LENGTH = 12;
+            if (index < MAX_LENGTH) {
+              return (
+                <Card
+                  key={ name }
+                  thumb={ image }
+                  name={ name }
+                  index={ index }
+                  id={ id }
+                  url="comidas"
+                />
+              );
+            }
+            return null;
+          })
+        }
+      </div>
 
-        <MenuInferior />
-      </>
+      <MenuInferior />
+    </>
 
-    );
-  }
+  );
 }
 
 export default Comes;
