5b5bc45223cdc3aecb357b8f45b78fd87d1a4a8c
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-01-13 22:48:16 -0300

    Updates

---
+++ precache-manifest.6bbe81c5c4686476c0d3e7329a666b92.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54c53905d7ee993841f3d24a8268894",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "44f09967a407fe03dcb7",
+    "url": "/recipes-app/static/css/2.945f44db.chunk.css"
+  },
+  {
+    "revision": "cea0b7c520e26e6ab6b5",
+    "url": "/recipes-app/static/css/main.ef3b0ea9.chunk.css"
+  },
+  {
+    "revision": "44f09967a407fe03dcb7",
+    "url": "/recipes-app/static/js/2.a2ca2fb0.chunk.js"
+  },
+  {
+    "revision": "d2b6c80184c0d9c48452c80dabc06517",
+    "url": "/recipes-app/static/js/2.a2ca2fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea0b7c520e26e6ab6b5",
+    "url": "/recipes-app/static/js/main.52ffbfd2.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "fed972920c028a3e1fc6065618e170c6",
+    "url": "/recipes-app/static/media/blackHeartIcon.fed97292.svg"
+  },
+  {
+    "revision": "e20c3bb888d5148078d29eff51b605bc",
+    "url": "/recipes-app/static/media/drinkIcon.e20c3bb8.svg"
+  },
+  {
+    "revision": "8486ec46bdd2f85a74769895b7d1ba2c",
+    "url": "/recipes-app/static/media/exploreIcon.8486ec46.svg"
+  },
+  {
+    "revision": "0a218dad3829d89bcf72675425a6b5fd",
+    "url": "/recipes-app/static/media/mealIcon.0a218dad.svg"
+  },
+  {
+    "revision": "8712a673584ce3ba8d57bfcba4860342",
+    "url": "/recipes-app/static/media/profileIcon.8712a673.svg"
+  },
+  {
+    "revision": "4552d4406cecc569cca33b4f1d54733f",
+    "url": "/recipes-app/static/media/searchIcon.4552d440.svg"
+  },
+  {
+    "revision": "bede90052e7ec3f6268f45752c94144b",
+    "url": "/recipes-app/static/media/shareIcon.bede9005.svg"
+  },
+  {
+    "revision": "8de3938be0319283a299e221212d6f70",
+    "url": "/recipes-app/static/media/wallpaper01.8de3938b.jpg"
+  },
+  {
+    "revision": "7e5b379e5ef0875091b4d7b0291f3822",
+    "url": "/recipes-app/static/media/whiteHeartIcon.7e5b379e.svg"
+  }
+]);
